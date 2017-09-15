cf481817947de1940da94ce36940385c6adc13c7
Xing Tao Shi <xshi@stuy.edu>
2017-09-15 13:27:20 -0400

    finished euleR

--- euler.c
+++ euler.c
@@ -16,11 +16,27 @@ int prob2(int biggest_num) {
   int sum = 0;
   int first = 1;
   int second = 2;
-  while ((!first > 4000000) || (!second > 4000000)); 
-  
+  sum += second;
+  while ((first < biggest_num) && (second < biggest_num)) {
+    if (first > second) {
+      second += first;
+      if (second % 2 == 0) {
+	sum += second;
+      }
+    }
+    else {
+      first += second;
+      if (first % 2 == 0) {
+	sum += first;
+      }
+    }
+  }
+  return sum;
 }
 
 int main() {
   int result_prob1 = prob1(1000);
   printf("The result of problem 1 is %d\n", result_prob1);
+  int result_prob2 = prob2(4000000);
+  printf("The result of problem 2 is %d\n", result_prob2);
 }
